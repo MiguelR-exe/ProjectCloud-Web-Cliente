ede637a1202f2ea21362ae75e7bc95c15f8136d6
Miguel <miguel.ramos@utec.edu.pe>
2026-06-27 17:49:31 -0500

    URL de API

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // === IMPORTANTE: pega aqui tu Invoke URL de API Gateway ===
 // Ejemplo: https://abc123.execute-api.us-east-1.amazonaws.com/dev
-export const API_URL = "https://TU-URL.execute-api.us-east-1.amazonaws.com/dev";
+export const API_URL = "https://d5qmtbtwn1.execute-api.us-east-1.amazonaws.com/dev";
 
 export const TENANT_ID = "madamtusan";
